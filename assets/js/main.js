e5d5b658753615ab19ae2c86179aa6f393d173d6
PauloEduardo-Ferreira <paulo.eduardoliveira@hotmail.com>
2025-07-02 17:58:21 -0300

    Adicionando o Projeto Pokedex

---
+++ assets/js/main.js
@@ -0,0 +1,62 @@
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
+const limit = 8;
+let offset = 0;
+
+function loadPokemonItens(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons
+      .map(
+        (pokemon) => `
+      <li class="pokemon ${pokemon.type}">
+        <div class="card-inner">
+          <div class="front">
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            <div class="detail">
+              <ol class="types">
+                ${pokemon.types
+                  .map((type) => `<li class="type ${type}">${type}</li>`)
+                  .join(" ")}
+              </ol>
+              <img
+                src="${pokemon.photo}"
+                alt="${pokemon.name}"
+              />
+            </div>
+          </div>
+
+          <div class="back">
+            <div class="extra-info">
+              <p><strong>Altura:</strong> ${(pokemon.height / 10).toFixed(1)} m</p>
+              <p><strong>Peso:</strong> ${(pokemon.weight / 10).toFixed(1)} kg</p>
+              <p><strong>Stats:</strong></p>
+              <ul>
+                ${pokemon.stats
+                  .map((stat) => `<li>${stat.name}: ${stat.value}</li>`)
+                  .join("")}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </li>
+    `
+      )
+      .join("");
+    pokemonList.innerHTML += newHtml;
+
+    const cards = document.querySelectorAll(".pokemon");
+    cards.forEach((card) => {
+      card.addEventListener("click", () => {
+        card.classList.toggle("active");
+      });
+    });
+  });
+}
+
+loadPokemonItens(offset, limit);
+
+loadMoreButton.addEventListener("click", () => {
+  offset += limit;
+  loadPokemonItens(offset, limit);
+});
---
+++ assets/js/pokeApi.js
@@ -0,0 +1,49 @@
+const pokeApi = {}
+
+function convertPokeApiDetailToPokemon(pokeDetail) {
+  const pokemon = new Pokemon();
+  pokemon.number = pokeDetail.order;
+  pokemon.name = pokeDetail.name;
+
+  const types = pokeDetail.types.map(typeSlot => typeSlot.type.name);
+  const [type] = types;
+
+  pokemon.types = types;
+  pokemon.type = type;
+
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+    || pokeDetail.sprites.front_default
+    || 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png'; // fallback imagem
+
+ 
+  pokemon.height = pokeDetail.height; 
+  pokemon.weight = pokeDetail.weight;
+
+  pokemon.abilities = pokeDetail.abilities.map(
+    abilitySlot => abilitySlot.ability.name
+  );
+
+  pokemon.stats = pokeDetail.stats.map(statSlot => ({
+    name: statSlot.stat.name,
+    value: statSlot.base_stat,
+  }));
+
+  return pokemon;
+}
+
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url)
+    .then(response => response.json())
+    .then(convertPokeApiDetailToPokemon);
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5) => {
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(jsonBody => jsonBody.results)
+    .then(pokemons => pokemons.map(pokeApi.getPokemonDetail))
+    .then(detailRequests => Promise.all(detailRequests))
+    .then(pokemonsDetails => pokemonsDetails);
+}
